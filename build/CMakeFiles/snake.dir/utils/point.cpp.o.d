ace84c6a2f1a4fb94ed6a51c9ccc79201c1539e5
Michal Dvořák <dvora125@fit.cvut.cz>
2022-08-15 14:21:14 +0200

    delete build

--- build/CMakeFiles/snake.dir/utils/point.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/snake.dir/utils/point.cpp.o: \
- /home/mdvorak/programming/projects/snake/utils/point.cpp \
- /usr/include/stdc-predef.h \
- /home/mdvorak/programming/projects/snake/utils/point.hpp \
- /home/mdvorak/programming/projects/snake/utils/vec2.hpp
--- build/CMakeFiles/snake.dir/utils/vec2.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/snake.dir/utils/vec2.cpp.o: \
- /home/mdvorak/programming/projects/snake/utils/vec2.cpp \
- /usr/include/stdc-predef.h \
- /home/mdvorak/programming/projects/snake/utils/vec2.hpp
